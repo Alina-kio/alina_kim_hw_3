af74dd65f85b77a9e83dadd4e97d206bbed382e2
Alina-kio <alinaolkim@gmail.com>
2023-01-20 00:43:14 +0600

    hw on ingenuity

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         double[] numbers = {1.5, 2.4, -2.5, 6.2, 4.6, 7.4, -1.8, 5.3, -8.1, 7.4, -5.9, 7.5, 4.1, -6.1, 9.9};
@@ -19,5 +21,25 @@ public class Main {
         }
         System.out.println();
         System.out.println("Среднее арифметическое " + average + " / " + count + " = " + average / count);
+
+
+
+//        ДЗ на сообразительность:
+        int[] arr = {8, -2, -4, 2, 3, 6};
+        int min;
+        for (int i = 0; i < arr.length; i++) {
+            min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            if (min != i) {
+                int n = arr[i];
+                arr[i] = arr[min];
+                arr[min] = n;
+                System.out.println(Arrays.toString(arr));
+            }
+        }
     }
 }
